b96263ecba80f88e2a6b853550340adf9b14ea35
Artur HRYTSENKO <ahrytsen@e1r9p2.unit.ua>
2018-09-12 19:34:48 +0300

    in progress

--- src/AVM.cpp
+++ src/AVM.cpp
@@ -1,25 +1,13 @@
 // ************************************************************************** //
 //                                                                            //
 //                                                        :::      ::::::::   //
-//   AVM.hpp                                            :+:      :+:    :+:   //
+//   AVM.cpp                                            :+:      :+:    :+:   //
 //                                                    +:+ +:+         +:+     //
 //   By: ahrytsen <ahrytsen@student.unit.ua>        +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
-//   Created: 2018/07/30 13:48:23 by ahrytsen          #+#    #+#             //
-//   Updated: 2018/09/12 18:52:32 by ahrytsen         ###   ########.fr       //
+//   Created: 2018/09/12 17:00:56 by ahrytsen          #+#    #+#             //
+//   Updated: 2018/09/12 18:22:35 by ahrytsen         ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
-#ifndef AVM_HPP
-# define AVM_HPP
-
-class	AVM
-{
-	std::istream			_in;
-	std::stack<IOperand>	_stack;
-
-	AVM();
-	AVM();
-};
-
-#endif
+#include <AVM.hpp>
--- src/OFactory.cpp
+++ src/OFactory.cpp
@@ -6,7 +6,7 @@
 //   By: ahrytsen <ahrytsen@student.unit.ua>        +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2018/09/11 15:38:27 by ahrytsen          #+#    #+#             //
-//   Updated: 2018/09/11 21:26:04 by ahrytsen         ###   ########.fr       //
+//   Updated: 2018/09/12 16:59:03 by ahrytsen         ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
@@ -27,23 +27,38 @@ OFactory &			OFactory::operator=( OFactory const & f ) {
 OFactory::~OFactory() {}
 
 IOperand const *	OFactory::createInt8( std::string const & value ) const {
-
+	try { int res = std::stoi(value); }
+	catch ( std::range_error & e) {}
+	catch ( std::invalid_argument & e) {}
+	return new TOperand<int8_t>();
 }
 
 IOperand const *	OFactory::createInt16( std::string const & value ) const {
-
+	try { int res = std::stoi(value); }
+	catch ( std::range_error & e) {}
+	catch ( std::invalid_argument & e) {}
+	return new TOperand<int8_t>();
 }
 
 IOperand const *	OFactory::createInt32( std::string const & value ) const {
-
+	try { int res = std::stoi(value); }
+	catch ( std::range_error & e) {}
+	catch ( std::invalid_argument & e) {}
+	return new TOperand<int8_t>();
 }
 
 IOperand const *	OFactory::createFloat( std::string const & value ) const {
-
+	try { int res = std::stoi(value); }
+	catch ( std::range_error & e) {}
+	catch ( std::invalid_argument & e) {}
+	return new TOperand<int8_t>();
 }
 
 IOperand const *	OFactory::createDouble( std::string const & value ) const {
-
+	try { int res = std::stoi(value); }
+	catch ( std::range_error & e) {}
+	catch ( std::invalid_argument & e) {}
+	return new TOperand<int8_t>();
 }
 
 const OFactory &	OFactory::getInstance() { return _instance; }
@@ -54,7 +69,7 @@ IOperand const *	OFactory::createOperand( eOperandType type, std::string const &
 		&OFactory::createFloat, &OFactory::createDouble
 	};
 	if (type < Int8 || type > _Double )
-		throw std::out_of_range ("Unknown type!");
+		throw std::out_of_range ("Unknown operand type!");
 	return ((this->*funcs[type])(value));
 }
 
